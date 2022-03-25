edbc5c02dd53715ea1fd4c8538870523de48f128
lightning-dog <1090044503@qq.com>
2022-03-25 16:32:06 +0800

    test submit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <tabBar>
+          <tabBarItem>
+            <img src="./assets/img/主页.png" slot='itemIcon'>
+            <div slot='itemText'>首页</div>
+          </tabBarItem>
+          <tabBarItem>
+            <img src="./assets/img/主页.png" slot='itemIcon'>
+            <div slot='itemText'>首页</div>
+          </tabBarItem>
+          <tabBarItem>
+            <img src="./assets/img/主页.png" slot='itemIcon'>
+            <div slot='itemText'>首页</div>
+          </tabBarItem>
+          <tabBarItem>
+            <img src="./assets/img/主页.png" slot='itemIcon'>
+            <div slot='itemText'>首页</div>
+          </tabBarItem>
+    </tabBar>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import tabBar from './components/tabBar/tabBar.vue'
+import tabBarItem from './components/tabBar/tabBarItem.vue'
+export default {
+  name: 'App',
+  components: {
+    tabBar,
+    tabBarItem
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/tabBar/tabBarItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="tabBarItem">
+        <slot name="itemIcon"></slot>
+        <slot name='itemText'></slot>
+    </div>
+</template>
+<script>
+
+export default {
+  name: 'tabBarItem'
+}
+</script>
+<style>
+.tabBarItem{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+}
+.tabBarItem img{
+    width: 24px;
+    height: 24px;
+    margin-bottom: -4px;
+}
+</style>
